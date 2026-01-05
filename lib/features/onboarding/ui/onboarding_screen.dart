976d0d9ac1d370d336968ccf61fb3ca77d3f39c7
Nesma <nesmaty2000@gmail.com>
2026-01-05 15:12:10 +0200

    - added images and svgs folders for assets - added flutter_svg & flutter_screenutil - made a file for doc_app client - added navigation extension - added routing folder that contains router class and routes file -  added colors & styles - added empty login screen - added onboarding screen

---
+++ lib/features/onboarding/ui/onboarding_screen.dart
@@ -0,0 +1,41 @@
+import 'package:docdoc/features/onboarding/ui/widgets/doc_logo_and_name.dart';
+import 'package:docdoc/features/onboarding/ui/widgets/doctor_image_and_text.dart';
+import 'package:docdoc/features/onboarding/ui/widgets/get_started_btn.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../core/theming/styles.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: SingleChildScrollView(
+            padding: EdgeInsets.only(top: 30.h, bottom: 30.h,),
+            child:  Column(
+              children: [
+                const DocLogoAndName(),
+                SizedBox(height: 30.h,),
+                const DoctorImageAndText(),
+                Padding(
+                  padding:  EdgeInsets.symmetric(horizontal: 30.w),
+                  child: Column(
+                    children: [
+                      Text('Manage and schedule all of your medical appointments easily with Docdoc to get a new experience.',
+                        style: TextStyles.font12Grey400Weight,
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 30.h),
+                const GetStartedBtn(),
+              ],
+            ),
+          )),
+    );
+  }
+}
