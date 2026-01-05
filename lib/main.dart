4a1bdf02f54ee418f023587d63b5a94d28a930a3
Nesma <nesmaty2000@gmail.com>
2026-01-05 15:20:33 +0200

    fake push into origin development

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,8 @@ import 'package:docdoc/doc_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(DocApp(appRouter: AppRouter(),));
+  runApp(
+      DocApp(
+    appRouter: AppRouter(),
+  ));
 }
