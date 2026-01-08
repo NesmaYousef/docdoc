d91f413fb4c3776651cf161a3f939debb568971d
Nesma <nesmaty2000@gmail.com>
2026-01-08 20:27:22 +0200

    - Added general textButton - Finished Login Screen design

---
+++ lib/features/login/ui/widgets/terms_and_conditions_text.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TermsAndConditionsText extends StatelessWidget {
+const TermsAndConditionsText({Key? key}) : super(key: key);
+
+@override
+Widget build(BuildContext context) {
+return Container();
+}
+}
