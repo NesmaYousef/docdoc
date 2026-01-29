b69bacc6ce4eaa73c0c6e9d9724732b5222bf797
Nesma <nesmaty2000@gmail.com>
2026-01-29 20:47:25 +0200

    - Added Home top bar

--- lib/features/home/ui/screens/home_screen.dart
+++ lib/features/home/ui/screens/home_screen.dart
@@ -2,12 +2,29 @@
 
 import 'package:flutter/material.dart';
 
+import '../widgets/home_top_bar.dart';
+
 class HomeScreen extends StatelessWidget {
 
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Home')));
+    return Scaffold(
+    backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          margin: EdgeInsets.fromLTRB(20, 16, 20, 28),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              HomeTopBar(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
+
---
+++ lib/features/home/ui/widgets/home_top_bar.dart
@@ -0,0 +1,40 @@
+import 'package:docdoc/core/theming/colors.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../../core/theming/styles.dart';
+
+class HomeTopBar extends StatelessWidget {
+  const HomeTopBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Hi, Nesma!',
+              style: TextStyles.font14DarkBlueBold,
+            ),
+            Text(
+              'How are you today?',
+              style: TextStyles.font12GrayRegular,
+            ),
+          ],
+        ),
+        Spacer(),
+        CircleAvatar(
+          radius: 24.r,
+          backgroundColor: ColorsManager.moreLighterGray,
+          child: SvgPicture.asset('assets/svgs/alert.svg'),
+        ),
+      ],
+    );
+  }
+}
